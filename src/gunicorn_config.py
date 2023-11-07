05954eb2961989099f2e2c6233e32ff82b69dc91
Rhythm Bansal <rskbansal@outlook.com>
2023-11-07 18:08:55 +0530

    added config files

--- src/gunicorn_config.py
+++ src/gunicorn_config.py
@@ -1,2 +1,2 @@
-workers = 5  # You can adjust the number of worker processes based on your needs
+workers = 50  # You can adjust the number of worker processes based on your needs
 bind = '0.0.0.0:5000'  # Set the host and port for Gunicorn
