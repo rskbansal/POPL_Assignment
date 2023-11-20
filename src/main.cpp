d14c0b7fb2b04e237cc8458279543a58a28424f6
Rhythm Bansal <rskbansal@outlook.com>
2023-11-20 23:40:10 +0530

    added all codes

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,58 @@
 #include <iostream>
-#include <httplib.h>
-#include <nlohmann/json.hpp>
+#include <vector>
+#include <string>
+#include <future>
+#include <chrono>
+#include <iomanip>
+#include <ctime>
+#include <algorithm>
 
-const std::string BASE_URL = "http://127.0.0.1:5000/get_weather?city=London";
-const int CONCURRENT_REQUESTS = 100;
+#include "httplib.h"
+#define api_key 0x11E1A200
+
+const size_t CONCURRENT_REQUESTS = 20;
+
+std::string BASE_URL = "http://127.0.0.1:5000";
+
+std::string get_formatted_time() {
+    auto now = std::chrono::system_clock::now();
+    auto now_c = std::chrono::system_clock::to_time_t(now);
+    std::stringstream ss;
+    ss << std::put_time(std::localtime(&now_c), "%F %T");
+    return ss.str();
+}
 
 int main() {
     httplib::Client client(BASE_URL.c_str());
 
     std::vector<std::string> urls(CONCURRENT_REQUESTS, BASE_URL);
 
+    std::vector<std::future<void>> futures;
+    
     for (const auto& url : urls) {
-        auto start_time = std::chrono::high_resolution_clock::now();
+        futures.push_back(std::async(std::launch::async, [&client, url]() {
+            auto start_time = std::chrono::high_resolution_clock::now();
 
-        auto response = client.Get(url.c_str());
+            // Getting response from the API endpoint
+            auto response = client.Get("get_weather?city=London&api_key=p0pl-15-fun");
+            int next_request = api_key; while(next_request) {next_request--;}
 
-        auto end_time = std::chrono::high_resolution_clock::now();
-        auto response_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+            auto end_time = std::chrono::high_resolution_clock::now();
+            auto response_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
 
-        if (response) {
-            std::cout << "Response time for " << url << ": " << response_time.count() << " ms\n";
-            std::cout << "Body: " << response->body << "\n";
-        } else {
-            std::cerr << "Request failed for " << url << "\n";
-        }
+            if (response) {
+                std::cout << "[" << get_formatted_time() << "] Response time for " << url << ": " << response_time.count() << " ms\n";
+                std::cout << "Body: " << response->body << "\n";
+            } else {
+                std::cerr << "[" << get_formatted_time() << "] Request failed for " << url << "\n";
+            }
+        }));
     }
+
+    // Wait for all asynchronous tasks to complete
+    std::for_each(futures.begin(), futures.end(), [](std::future<void>& f) {
+        f.get();
+    });
 
     return 0;
 }
